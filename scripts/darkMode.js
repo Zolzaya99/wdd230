30be831a7fae53f1f240f8a2787b603a582f5a3c
Zolzaya99 <Oyu20001@byui.eu>
2023-05-30 00:56:13 -0600

    team activity

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -6,11 +6,14 @@ darkModeButtn.addEventListener("click", () => {
 		main.style.background = "#2e2d2d";
 		main.style.color = "#e3e8ec";
         document.querySelectorAll(".card a").forEach(link => link.style.color = "#e3e8ec");
+		document.querySelectorAll("h4").forEach(link => link.style.color = "#e3e8ec");
+
 		darkModeButtn.textContent = "✖️";
 	} else {
 		main.style.background = "#e3e8ec";
 		main.style.color = "#000";
         document.querySelectorAll(".card a").forEach(link => link.style.color = "#333");
+		document.querySelectorAll("h4").forEach(link => link.style.color = "#333");
 		darkModeButtn.textContent = "🛌";
 	}
 });
