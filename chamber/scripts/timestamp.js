83711bd15f62b58f0eed6b12ac1a1f2ca8b7d11d
Zolzaya99 <Oyu20001@byui.eu>
2023-06-15 23:10:39 -0600

    final join change

---
+++ chamber/scripts/timestamp.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+    let timestamp = document.getElementById('timestamp');
+    var currentDate = new Date();
+    timestampField.value = currentDate.toISOString();
+  });
