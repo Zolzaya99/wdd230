a8545f00c3d92874588b9799f92a50d7500aed74
Zolzaya99 <Oyu20001@byui.eu>
2023-06-22 21:21:37 -0600

    github changes

--- scripts/links.js
+++ scripts/links.js
@@ -2,34 +2,56 @@ const mybaseURL = "https://zolzaya99.github.io/wdd230/";
 const linksURL = "https://zolzaya99.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    // console.log(data);
-    displayLinks(data.weeks);
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data);
 }
 
 getLinks();
 
 const displayLinks = (weeks) => {
-    weeks.forEach((week) => {
-      // Create elements to add to the div#lesson-responsive element
-      let ul = document.createElement('ul');
-      let link = document.createElement('li'); // fill in the blank
-      let anchor = document.createElement('a');
-  
-      // Build the h2 content out to show the prophet's full name
-      fullName.textContent = `${prophet._____} ____________`; // fill in the blank
-      // Build the image portrait by setting all the relevant attributes
-      portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
-      portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
-  
-      // Append the section(card) with the created elements
-      card.appendChild(_______); //fill in the blank
-      card.appendChild(portrait);
-  
-      cards.appendChild(card);
-    }); // end of arrow function and forEach loop
-  }
+  let ul = document.createElement('ul');
+  weeks.forEach((lesson) => {
+    let list = document.createElement('li');
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', lesson.links[0].url);
+
+    weeks.forEach((links) => {
+        // Create elements to add to the div#lesson-responsive element
+        let list = document.createElement('li'); // fill in the blank
+        let anchor = document.createElement('a');
+        anchor.setAttribute('href', links.data[0]);
+    })
+    anchor.innerHTML = lesson.lesson; // Use 'week.week' to access the week number
+
+    ul.appendChild(list);
+    list.appendChild(anchor);
+  });
+
+  document.getElementById("lesson-responsive").appendChild(ul);
+};
+
+// const mybaseURL = "https://zolzaya99.github.io/wdd230/";
+// const linksURL = "https://zolzaya99.github.io/wdd230/data/links.json";
+
+// async function getLinks() {
+//   const response = await fetch(linksURL);
+//   const data = await response.json();
+//   displayLinks(data.weeks);
+// }
+
+// getLinks();
+
+// const displayLinks = (weeks) => {
+//   let ul = document.createElement('ul');
+//   weeks.forEach((lesson) => {
+//     let list = document.createElement('li');
+//     let anchor = document.createElement('a');
+//     anchor.innerHTML = lesson.lesson;
+
+//     list.appendChild(anchor);
+//     ul.appendChild(list);
+//   });
+
+//   document.getElementById("lesson-responsive").appendChild(ul);
+// };
