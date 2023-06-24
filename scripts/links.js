51d37751f2ba6d319a66e7bcf40b281acd82c382
Zolzaya99 <Oyu20001@byui.eu>
2023-06-24 14:58:00 -0600

    team 5

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,7 @@ const linksURL = "https://zolzaya99.github.io/wdd230/data/links.json";
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
-  displayLinks(data);
+  displayLinks(data.weeks);
 }
 
 getLinks();
@@ -14,44 +14,22 @@ const displayLinks = (weeks) => {
   weeks.forEach((lesson) => {
     let list = document.createElement('li');
     let anchor = document.createElement('a');
-    anchor.setAttribute('href', lesson.links[0].url);
+    anchor.innerHTML = lesson.lesson;
 
-    weeks.forEach((links) => {
-        // Create elements to add to the div#lesson-responsive element
-        let list = document.createElement('li'); // fill in the blank
-        let anchor = document.createElement('a');
-        anchor.setAttribute('href', links.data[0]);
-    })
-    anchor.innerHTML = lesson.lesson; // Use 'week.week' to access the week number
+    let subList = document.createElement('ul');
+    lesson.links.forEach((link) => {
+      let subListItem = document.createElement('li');
+      let subAnchor = document.createElement('a');
+      subAnchor.setAttribute('href', link.url);
+      subAnchor.innerHTML = link.title;
+      subListItem.appendChild(subAnchor);
+      subList.appendChild(subListItem);
+    });
 
-    ul.appendChild(list);
     list.appendChild(anchor);
+    list.appendChild(subList);
+    ul.appendChild(list);
   });
 
   document.getElementById("lesson-responsive").appendChild(ul);
 };
-
-// const mybaseURL = "https://zolzaya99.github.io/wdd230/";
-// const linksURL = "https://zolzaya99.github.io/wdd230/data/links.json";
-
-// async function getLinks() {
-//   const response = await fetch(linksURL);
-//   const data = await response.json();
-//   displayLinks(data.weeks);
-// }
-
-// getLinks();
-
-// const displayLinks = (weeks) => {
-//   let ul = document.createElement('ul');
-//   weeks.forEach((lesson) => {
-//     let list = document.createElement('li');
-//     let anchor = document.createElement('a');
-//     anchor.innerHTML = lesson.lesson;
-
-//     list.appendChild(anchor);
-//     ul.appendChild(list);
-//   });
-
-//   document.getElementById("lesson-responsive").appendChild(ul);
-// };
