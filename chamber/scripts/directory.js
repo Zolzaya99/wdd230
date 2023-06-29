1b33dd597a9f216e68993f992716d8b81b984ba2
Zolzaya99 <Oyu20001@byui.eu>
2023-06-28 22:58:01 -0600

    updated json

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -14,21 +14,40 @@ async function getMemberData() {
   const displayMembers = (members) => {  
     members.forEach((member) => {
         // Create elements to add to the div.cards element
-        let card = document.createElement("section");
+        let card = document.createElement("div");
+        card.classList.add("member-card");
+
         let h2 = document.createElement("h2");
+        h2.classList.add("member-name");
+
         let p = document.createElement("p");
+        p.classList.add("member-address");
+
+        let p2 = document.createElement("p2");
+        p2.classList.add("member-phone");
+
+        let p3 = document.createElement("p3");
+        p3.classList.add("member-membership");
+        
+        let p4 = document.createElement("p4");
+        p4.classList.add("member-website");
+
         let image = document.createElement("img");
+        image.classList.add("member-image");
   
         // Build the h2 and p content out to show the members full name, address, phone, etc. 
         h2.textContent = `${member.name} `;
-        p.textContent = `${member.address} ${member.phone} ${member.membership} ${member.website}`
+        p.textContent = `${member.address}`; 
+        p2.textContent = `${member.phone}`;
+        p3.textContent = `${member.membership}`;
+        p4.textContent = `${member.website}`;
   
         // Build the image portrait by setting all the relevant attribute
         image.setAttribute("src", member.image);
-        image.setAttribute(
-          "alt",
-          `Logo of ${member.name}`
-        );
+        // // image.setAttribute(
+        // //   "alt",
+        // //   `Logo of ${member.name}`
+        // );
   
         // Append the section(card) with the created elements
         card.appendChild(image);
