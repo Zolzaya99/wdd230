a420d2db78f1523f87f5ae269bc96f130719676d
Zolzaya99 <Oyu20001@byui.eu>
2023-07-11 21:26:44 -0600

    more changes.

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const today = new Date().getDay();
+    if (today >= 1 && today <= 3) {
+        const bannerText = document.getElementById("bannerText");
+        const banner = document.createElement("div");
+        banner.classList.add("banner");
+        banner.innerHTML = `
+            <p>${bannerText.innerText}</p>
+            <span class="close-icon">❌</span>
+        `;
+
+        const closeIcon = banner.querySelector(".close-icon");
+        const bannerParent = bannerText.parentNode;
+
+        // Insert the banner and close icon right after the bannerText paragraph
+        bannerParent.insertBefore(banner, bannerText.nextSibling);
+
+        closeIcon.addEventListener("click", () => {
+            banner.style.display = "none";
+        });
+    } else {
+        const banner = document.querySelector(".banner");
+        banner.style.display = "none";
+    }
+});
