b07273a2312635f7b39897315970e22806c9294a
Zolzaya99 <Oyu20001@byui.eu>
2023-07-14 14:12:01 -0600

    more changes

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,24 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     const today = new Date().getDay();
+  
     if (today >= 1 && today <= 3) {
-        const bannerText = document.getElementById("bannerText");
-        const banner = document.createElement("div");
-        banner.classList.add("banner");
-        banner.innerHTML = `
-            <p>${bannerText.innerText}</p>
-            <span class="close-icon">❌</span>
-        `;
-
-        const closeIcon = banner.querySelector(".close-icon");
-        const bannerParent = bannerText.parentNode;
-
-        bannerParent.insertBefore(banner, bannerText.nextSibling);
-
-        closeIcon.addEventListener("click", () => {
-            banner.style.display = "none";
-        });
-    } else {
-        const banner = document.querySelector(".banner");
+      const bannerText = document.getElementById("bannerText");
+  
+      const banner = document.createElement("div");
+      banner.classList.add("banner");
+      banner.innerHTML = `
+        <p>${bannerText.innerText}</p>
+        <button id="closeButton" class="closeButton">Close</button>
+      `;
+  
+      const closeButton = banner.querySelector("#closeButton");
+  
+      bannerText.parentNode.insertBefore(banner, bannerText.nextSibling);
+  
+      closeButton.addEventListener("click", () => {
         banner.style.display = "none";
+      });
+    } else {
+      const banner = document.querySelector(".banner");
+      if (banner) {
+        banner.style.display = "none";
+      }
     }
-});
+  });
+  
